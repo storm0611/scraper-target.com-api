92d1a42d447402ef5a82fc67b1fe24c03aed8562
devstar0611 <devstar0611@gmail.com>
2022-07-04 03:40:16 -0700

    Update scraper_test.py

--- scraper_test.py
+++ scraper_test.py
@@ -45,13 +45,13 @@ soup = BeautifulSoup(html, "html.parser")
 # print(prodcut_upc)
 # product_image = soup.select('button[data-test="product-carousel-item-0"] img')[0]['src']
 # print(product_image)
+# product_category = soup.select('.PWWrr:nth-child(2) > span > a > span')[0].text
+# print(product_category)
 
-# product_price = soup.select('span[data-test="product-random-weight-price"]')
-# print(product_price)
+product_price = soup.select('span[data-test="product-random-weight-price"]')
+print(product_price)
 # product_description = soup.select('div[data-test="item-details-discription"]')
 # print(product_description)
-product_category = soup.select('.PWWrr:nth-child(2) > span > a > span')[0].text
-print(product_category)
 
 
 

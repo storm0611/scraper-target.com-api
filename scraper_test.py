81372caf81d2a94a56e23ac4bdd6c84270ad3776
devstar0611 <devstar0611@gmail.com>
2022-07-04 08:49:18 -0700

    Update scraper_test.py

--- scraper_test.py
+++ scraper_test.py
@@ -50,7 +50,7 @@ soup = BeautifulSoup(html, "html.parser")
 # product_description = soup.find_all("h3", string="Description")[0].parent.div.string
 # print(product_description)
 
-product_price = soup.find_all(attrs={'data-test': "product-random-weight-price"})
+product_price = soup.select('span.kfATIS')
 print(product_price)
 
 

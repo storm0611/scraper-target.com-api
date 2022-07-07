653dc51c8f7f7d8eefd2814ed7f7a6090b11efc1
devstar0611 <devstar0611@gmail.com>
2022-07-07 04:20:34 -0700

    Update scraper_test.py

--- scraper_test.py
+++ scraper_test.py
@@ -19,27 +19,28 @@ async def update_db():
     
     conn = sqlite3.connect('mydb.db')
     cur = conn.cursor()
-    
+    today = datetime.datetime.today().strftime("%Y-%m-%d")
+    # start - get all information of all products from target.com
     sys.stdout = open('log.txt', 'w+')
     print("---------------------------------------------------------------------------------------")
     print(datetime.datetime.today())
     print("====================================================================================")
-    
+
     categories = []
     subcategories = []
     sub_subcategories = []
     products = []
     products_count = []
     browser = await pyppeteer.launch(
-                                handleSIGINT=False,
-                                handleSIGTERM=False,
-                                handleSIGHUP=False)
+        handleSIGINT=False,
+        handleSIGTERM=False,
+        handleSIGHUP=False)
     page = await browser.newPage()
-    
+
     await page.goto("https://www.target.com/c/shop-all-categories/-/N-5xsxf")
     content = await page.content()
     soup = bs4.BeautifulSoup(content, features="lxml")
-    
+
     # start - finding categories list
     components = soup.select('div[data-component-type="Browse - Manual"]')
     print(len(components))
@@ -55,12 +56,14 @@ async def update_db():
         for category in children:
             category_name = category.a.text
             category_url = category.a['href']
+            if category_url.find("http") < 0:
+                category_url = TARGET_HOMEPAGE + category_url                
             print(category_name, category_url)
             categories.append({
                 'name': category_name,
                 'url': category_url
             })
-    
+
     # categories = json.load(open(os.path.join("categories.json")))
     # print(categories)
     jsonString = json.dumps(categories)
@@ -68,16 +71,13 @@ async def update_db():
     jsonFile.write(jsonString)
     jsonFile.close()
     # end - finding categories list
-    
+
     # start - finding subcategories or products list
     for category in categories:
         print("category=", category)
         category_name = category['name']
         category_url = category['url']
-        if category_url.find("http") < 0:
-            await page.goto(TARGET_HOMEPAGE + category_url)
-        else:
-            await page.goto(category_url)
+        await page.goto(category_url)
         await asyncio.sleep(5)
         content = await page.content()
         soup = bs4.BeautifulSoup(content, features="lxml")
@@ -100,7 +100,7 @@ async def update_db():
                 except:
                     break
                 # print(category_url + "?Nao=" + str(page_num * 24))
-                await page.goto(TARGET_HOMEPAGE + category_url + "?Nao=" + str(page_num * 24))
+                await page.goto(category_url + "?Nao=" + str(page_num * 24))
                 await asyncio.sleep(5)
                 content = await page.content()
                 soup = bs4.BeautifulSoup(content, features="lxml")
@@ -121,6 +121,8 @@ async def update_db():
                         product_url = item.a['href']
                     except:
                         continue
+                    if product_url.find("http") < 0:
+                        product_url = TARGET_HOMEPAGE + product_url
                     products.append({
                         "category": product_category,
                         "name": product_name,
@@ -165,6 +167,8 @@ async def update_db():
                             subcategory_url = subcategory.a['href']
                         except:
                             continue
+                        if subcategory_url.find("http") < 0:
+                            subcategory_url = TARGET_HOMEPAGE + subcategory_url
                         subcategories.append({
                             'parent': category_name,
                             'name': subcategory_name,
@@ -177,7 +181,7 @@ async def update_db():
                         })
             print("end - finding subcategories in category")
             # end - finding subcategories
-        
+
         print("end - finding in category")
         # end - finding subcategories or products list
     print("subcategories=", subcategories)
@@ -193,7 +197,7 @@ async def update_db():
             subcategory_name = category['name']
             subcategory_url = category['url']
             print("subcategory=", category)
-            await page.goto(TARGET_HOMEPAGE + subcategory_url)
+            await page.goto(subcategory_url)
             await asyncio.sleep(5)
             content = await page.content()
             soup = bs4.BeautifulSoup(content, features="lxml")
@@ -215,7 +219,7 @@ async def update_db():
                     except:
                         break
                     # print(subcategory_url + "?Nao=" + str(page_num * 24))
-                    await page.goto(TARGET_HOMEPAGE + subcategory_url + "?Nao=" + str(page_num * 24))
+                    await page.goto(subcategory_url + "?Nao=" + str(page_num * 24))
                     await asyncio.sleep(5)
                     content = await page.content()
                     soup = bs4.BeautifulSoup(content, features="lxml")
@@ -236,6 +240,7 @@ async def update_db():
                             product_url = item.a['href']
                         except:
                             continue
+                        product_url = TARGET_HOMEPAGE + product_url
                         products.append({
                             "category": product_category,
                             "name": product_name,
@@ -280,6 +285,8 @@ async def update_db():
                                 sub_subcategory_url = subcategory.a['href']
                             except:
                                 continue
+                            if sub_subcategory_url.find("http") < 0:
+                                sub_subcategory_url = TARGET_HOMEPAGE + sub_subcategory_url
                             sub_subcategories.append({
                                 'parent': category_name,
                                 'subcategory': subcategory_name,
@@ -303,7 +310,7 @@ async def update_db():
     # end - finding sub-subcategories or products list in subcategories
     if len(sub_subcategories):
         print("sub_subcategories not empty")
-    
+
     # start - get information from products url
     if len(products):
         jsonString = json.dumps(products)
@@ -345,14 +352,15 @@ async def update_db():
             content = await page.content()
             soup = bs4.BeautifulSoup(content, features="lxml")
             if len(soup.select('div[data-test="productNotFound"]')):
-                products.append({"upc": product_upc,
-                                "product_name": product_name,
-                                "product_price": product_price.replace('$', ''),
-                                "product_image": product_imageurl,
-                                "product_description": product_description,
-                                "product_category": product_category
-                                })
-                
+                products.append({"product_upc": product_upc,
+                                 "product_url": target_url,
+                                 "product_name": product_name,
+                                 "product_price": product_price.replace('$', ''),
+                                 "product_image": product_imageurl,
+                                 "product_description": product_description,
+                                 "product_category": product_category
+                                 })
+
                 # return {"upc": product_upc,
                 #         "product_name": product_name,
                 #         "product_price": product_price.replace('$', ''),
@@ -369,7 +377,8 @@ async def update_db():
                     soup = bs4.BeautifulSoup(content, features="lxml")
                     price = soup.select('span[data-test="product-random-weight-price"]')
                 if not len(price):
-                    products.append({"upc": product_upc,
+                    products.append({"product_upc": product_upc,
+                                     "product_url": target_url,
                                      "product_name": product_name,
                                      "product_price": product_price.replace('$', ''),
                                      "product_image": product_imageurl,
@@ -389,97 +398,99 @@ async def update_db():
                     # print(product_category)
                     product_description = soup.find_all("h3", string="Description")[0].parent.div.text
                     print(product_description)
-                    products.append({"upc": product_upc,
+                    products.append({"product_upc": product_upc,
+                                     "product_url": target_url,
                                      "product_name": product_name,
                                      "product_price": product_price.replace('$', ''),
                                      "product_image": product_imageurl,
                                      "product_description": product_description,
                                      "product_category": product_category
                                      })
-        print("product=", {"upc": product_upc,
+        print("product=", {"product_upc": product_upc,
+                           "product_url": target_url,
                            "product_name": product_name,
                            "product_price": product_price.replace('$', ''),
                            "product_image": product_imageurl,
                            "product_description": product_description,
                            "product_category": product_category
                            })
-    
     await browser.close()
-    conn.close()
-    
+    # end - get all information of all products from target.com
+    # insert new product and update old products in database
+    for product in products:
+        product_upc = product["product_upc"]
+        product_url = product["product_url"]
+        product_name = product["product_name"]
+        product_price = float(product["product_price"])
+        product_imageurl = product["product_image"]
+        product_description = product["product_description"]
+        product_category = product["product_category"]
+        if product_upc == 'Not Found':
+            # current product is unavailable
+            sql_query = "SELECT * FROM products WHERE url=" + "'" + product_url + "'"
+            if len(cur.execute(sql_query).fetchall()):
+                # it is existed in database
+                sql_query = "UPDATE products SET " + \
+                    "update_date=" + "'" + today + "', " +  \
+                    "close_date=" + "'" + today + "', " +  \
+                    "is_available=0" + ", " +  \
+                    " WHERE url=" + "'" + product_url + "'"
+                print(sql_query)
+                cur.execute(sql_query)
+                conn.commit()
+            else:
+                # it is not existed in database
+                sql_query = "INSERT INTO products " + \
+                            " (url, upc, name, description, image, price, category, open_date, update_date, close_cate, is_available) VALUES (" + \
+                            "'" + product_url + "', " + \
+                            '"' + product_upc + '", ' + \
+                            '"' + product_name + '", ' + \
+                            '"' + product_description + '", ' + \
+                            "'" + product_imageurl + "', " + \
+                            str(product_price) + ", " + \
+                            '"' + product_category + '", ' + \
+                            '"' + today + '", ' + \
+                            '"' + today + '", ' + \
+                            '"' + today + '", ' + \
+                            '0);'
+                print(sql_query)
+                cur.execute(sql_query)
+                conn.commit()
+        else:
+            # current product is available
+            sql_query = "SELECT * FROM products WHERE upc=" + "'" + product_upc + "'"
+            if len(cur.execute(sql_query).fetchall()):
+                # it is existed in database
+                sql_query = "UPDATE products SET " + \
+                            "name=" + "'" + product_name + "'" + ", " + \
+                            "description=" + "'" + product_description + "'" + ", " + \
+                            "image=" + "'" + product_imageurl + "', " +  \
+                            "category=" + "'" + product_category + "', " +  \
+                            "price=" + str(product_price) + ", " +  \
+                            "update_date=" + "'" + today + "', " +  \
+                            " WHERE upc=" + "'" + product_upc + "'"
+                print(sql_query)
+                cur.execute(sql_query)
+                conn.commit()
+            else:
+                # it is not existed in database = new data
+                sql_query = "INSERT INTO products " + \
+                            " (url, upc, name, description, image, price, category, open_date, update_date) VALUES (" + \
+                            "'" + product_url + "', " + \
+                            '"' + product_upc + '", ' + \
+                            '"' + product_name + '", ' + \
+                            '"' + product_description + '", ' + \
+                            "'" + product_imageurl + "', " + \
+                            str(product_price) + ", " + \
+                            '"' + product_category + '", ' + \
+                            '"' + today + '", ' + \
+                            '"' + today + '"' + \
+                            ');'
+                print(sql_query)
+                cur.execute(sql_query)
+                conn.commit()
 
-    
-async def get_price_name(target_url):
-    print("def get_price_name(target_url): " + target_url)
-    product_name = 'Not Found'
-    product_price = 'Not Found'
-    product_description = 'Not Found'
-    product_category = 'Not Found'
-    product_upc = 'Not Found'
-    product_imageurl = 'Not Found'
-    if not target_url:
-        return {"upc": product_upc,
-                "product_name": product_name,
-                "product_price": product_price.replace('$', ''),
-                "product_image": product_imageurl,
-                "product_description": product_description,
-                "product_category": product_category
-                }
-    browser = await pyppeteer.launch(handleSIGINT=False,
-                                     handleSIGTERM=False,
-                                     handleSIGHUP=False)
-    page = await browser.newPage()
-    await page.goto(target_url)
-    await asyncio.sleep(5)
-    content = await page.content()
-    soup = bs4.BeautifulSoup(content, features="lxml")
-    if len(soup.select('div[data-test="productNotFound"]')):
-        await browser.close()
-        return {"upc": product_upc,
-                "product_name": product_name,
-                "product_price": product_price.replace('$', ''),
-                "product_image": product_imageurl,
-                "product_description": product_description,
-                "product_category": product_category
-                }
-    price = soup.select('span[data-test="product-random-weight-price"]')
-    # print(price)
-    while not len(price):
-        content = await page.content()
-        soup = bs4.BeautifulSoup(content, features="lxml")
-        if len(soup.select('div[data-test="productNotFound"]')):
-            await browser.close()
-            return {"upc": product_upc,
-                "product_name": product_name,
-                "product_price": product_price.replace('$', ''),
-                "product_image": product_imageurl,
-                "product_description": product_description,
-                "product_category": product_category
-                }
-        price = soup.select('span[data-test="product-random-weight-price"]')
-        # print(price)
-        if len(price):
-            product_price = price[0].text
-            print(product_price)
-        await asyncio.sleep(1)
-    product_name = soup.select('h1[data-test="product-title"] span')[0].text
-    print(product_name)
-    product_upc = soup.find_all("b", string="UPC")[0].parent.text.split(' ')[1]
-    print(product_upc)
-    product_imageurl = soup.select('button[data-test="product-carousel-item-0"] img')[0]['src']
-    print(product_imageurl)
-    product_category = soup.select('.PWWrr:nth-child(2) > span > a > span')[0].text
-    print(product_category)
-    product_description = soup.find_all("h3", string="Description")[0].parent.div.text
-    print(product_description)
-    await browser.close()
-    return {"upc": product_upc,
-            "product_name": product_name,
-            "product_price": product_price.replace('$', ''),
-            "product_image": product_imageurl,
-            "product_description": product_description,
-            "product_category": product_category
-            }
+    conn.close()
 
 
 asyncio.run(update_db())

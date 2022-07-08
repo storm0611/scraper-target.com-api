40f55bf083ff0e7f34d3f60c242299080d2327ca
devstar0611 <devstar0611@gmail.com>
2022-07-08 10:57:05 -0700

    Update scraper_test.py

--- scraper_test.py
+++ scraper_test.py
@@ -86,7 +86,7 @@ async def update_db():
         if len(products_grid):
             # start - finding products in category
             print("start - finding products in category")
-            print(soup.select('h2[data-test="resultsHeading"]'))
+            # print(soup.select('h2[data-test="resultsHeading"]'))
             try:
                 results_count = int(soup.select('h2[data-test="resultsHeading"]')[0].text.split(" ")[0])
             except:

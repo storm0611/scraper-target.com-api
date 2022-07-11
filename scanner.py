797674c87813262133d2c6c8c121d30a0ec0bd09
devstar0611 <devstar0611@gmail.com>
2022-07-11 12:24:23 -0700

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -15,37 +15,37 @@ def BarcodeReader():
     print(frame)
     if not ret:
         return 0
-    time.sleep(2)
-    cv2.imshow(frame)
-    # Decode the barcode image
-    detectedBarcodes = decode(frame)
+    # time.sleep(2)
+    # cv2.imshow(frame)
+    # # Decode the barcode image
+    # detectedBarcodes = decode(frame)
 
-    # If not detected then print the message
-    if not detectedBarcodes:
-        print("Barcode Not Detected or your barcode is blank/corrupted!")
-    else:
+    # # If not detected then print the message
+    # if not detectedBarcodes:
+    #     print("Barcode Not Detected or your barcode is blank/corrupted!")
+    # else:
 
-        # Traverse through all the detected barcodes in image
-        for barcode in detectedBarcodes:
+    #     # Traverse through all the detected barcodes in image
+    #     for barcode in detectedBarcodes:
 
-            # Locate the barcode position in image
-            (x, y, w, h) = barcode.rect
+    #         # Locate the barcode position in image
+    #         (x, y, w, h) = barcode.rect
 
-            # Put the rectangle in image using
-            # cv2 to heighlight the barcode
-            cv2.rectangle(frame, (x-10, y-10),
-                          (x + w+10, y + h+10),
-                          (255, 0, 0), 2)
+    #         # Put the rectangle in image using
+    #         # cv2 to heighlight the barcode
+    #         cv2.rectangle(frame, (x-10, y-10),
+    #                       (x + w+10, y + h+10),
+    #                       (255, 0, 0), 2)
 
-            if barcode.data != "":
+    #         if barcode.data != "":
 
-                # Print the barcode data
-                print(barcode.data)
-                print(barcode.type)
+    #             # Print the barcode data
+    #             print(barcode.data)
+    #             print(barcode.type)
 
-    #Display the image
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # #Display the image
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
     cap.release()
     return deviceID
 

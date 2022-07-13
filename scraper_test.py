a04024e9ea012ac734b644f67f2bec8e0b269bbe
devstar0611 <devstar0611@gmail.com>
2022-07-13 03:11:33 -0700

    Update scraper_test.py

--- scraper_test.py
+++ scraper_test.py
@@ -621,7 +621,192 @@ today = datetime.datetime.today().strftime("%Y-%m-%d")
 conn = sqlite3.connect('mydb.db')
 cur = conn.cursor()
 
+table_name = ""
+
 def insert_into_table(product_info):
+
+    global table_name
+
+    disc = "0"
+    sql_query = "SELECT discount FROM discounts WHERE category_name=" + \
+        "'" + product_info['category'] + "'"
+    print(sql_query)
+    results = cur.execute(sql_query).fetchall()
+    # print(results)
+    if len(results):
+        disc = str(results[0][0])
+    sql_query = "SELECT * FROM " + table_name + " WHERE tcin=" + \
+        "'" + product_info['tcin'] + "'"
+    print(sql_query)
+    results = cur.execute(sql_query).fetchall()
+    if len(results):
+        id = results[0][0]
+        sql_query = "UPDATE " + table_name + " SET " + \
+                    "price=" + "'" + product_info['price_min'] + "', " +  \
+                    "disc=" + "'" + disc + "', " +  \
+                    "update_date=" + "'" + today + "' " +  \
+                    " WHERE id=" + str(id)
+        print(sql_query)
+        cur.execute(sql_query)
+        conn.commit()
+    else:
+        sql_query = 'INSERT INTO ' + table_name + ' (url, tcin, name, description, image, category, price, disc, employee, open_date, update_date) ' + \
+            " VALUES (" + \
+            '"' + product_info['url'] + '", ' + \
+            '"' + product_info['tcin'] + '", ' + \
+            '"' + product_info['name'] + '", ' + \
+            '"' + product_info['description'] + '", ' + \
+            '"' + product_info['image'] + '", ' + \
+            '"' + product_info['category'] + '", ' + \
+            '"' + product_info['price_min'] + '", ' + \
+            '"' + disc + '", ' + \
+            '"' + product_info['employee'] + '", ' + \
+            '"' + today + '", ' + \
+            '"' + today + '"' + ");"
+        print(sql_query)
+        cur.execute(sql_query)
+        conn.commit()
+
+def get_products_category(categories):
+
+    global table_name
+
+    count = 28
+    products_info = []
+    for category in categories:
+        print("category=", category)
+        category_name = category['name']
+        table_name = category_name + "_products"
+        print(table_name)
+        sql_query = "CREATE TABLE IF NOT EXISTS " + table_name + \
+                    " ( id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, " + \
+                    "url TEXT," + \
+                    "tcin TEXT," + \
+                    "upc TEXT," + \
+                    "name TEXT," + \
+                    "description TEXT," + \
+                    "image TEXT," + \
+                    "category TEXT," + \
+                    "price TEXT," + \
+                    "disc TEXT," + \
+                    "stock TEXT," + \
+                    "employee TEXT," + \
+                    "open_date TEXT," + \
+                    "update_date TEXT," + \
+                    "is_available integer DEFAULT 1," + \
+                    "last_sold TEXT," + \
+                    "last_price TEXT);"
+        print(sql_query)
+        cur.execute(sql_query)
+        conn.commit()
+        category_url = category['url']
+        category_id = category_url.split('-')[-1]
+        print("category_id=", category_id)
+        current_page = total_pages = 1
+        total_results = 0
+        offset = 0
+        cnt = 0
+        time_one_page = datetime.timedelta(seconds=0)
+        while current_page <= total_pages:
+            if cnt > total_results:
+                break
+            params3 = {
+                "key": API_KEY,
+                "category": category_id,
+                "channel": "WEB",
+                "count": str(count),
+                "default_purchasability_filter": "false",
+                "include_sponsored": "true",
+                "offset": str(offset),
+                "page": "%2Fs%2F" + category_id,
+                "platform": "desktop",
+                "pricing_store_id": "3991",
+                "useragent": "Mozilla%2F5.0+%28Windows+NT+10.0%3B+Win64%3B+x64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F103.0.0.0+Safari%2F537.36",
+                "visitor_id": "0181DBA81F220201B2C4F5C04CBA071E"
+            }
+            # start_time = datetime.datetime.now()
+            if time_one_page.total_seconds() < 5:
+                time.sleep(5 - time_one_page.total_seconds())
+            response = requests.get(API_URL1, params=params3)
+            # print("category response time=", datetime.datetime.now() - start_time)
+            start_time = datetime.datetime.now()
+            if response.status_code > 300:
+                print("category_requests:", response.status_code)
+                time.sleep(5)
+                break
+            searched = response.json()['data']['search']
+            overview = searched['search_response']["typed_metadata"]
+            print("overview=", overview)
+            current_page = overview['current_page']
+            offset = overview['offset']
+            total_pages = overview['total_pages']
+            total_results = overview['total_results']
+            products = searched['products']
+            print("len(products)=", len(products))
+            if not len(products):
+                print("products not found")
+                offset += count
+                time.sleep(5)
+                break
+            for product in products:
+                try:
+                    url = product['item']['enrichment']['buy_url']
+                except:
+                    url = 'Not Found'
+                try:
+                    image = product['item']['enrichment']['images']['primary_image_url']
+                except:
+                    image = 'Not Found'
+                try:
+                    vender = product['item']['product_vendors'][0]['vendor_name']
+                except:
+                    vender = 'Not Found'
+                try:
+                    name = product['item']['product_description']['title']
+                except:
+                    name = 'Not Found'
+                try:
+                    description = ''.join(str(
+                        sub) for sub in product['item']['product_description']['soft_bullets']['bullets'])
+                except:
+                    description = 'Not Found'
+                try:
+                    tcin = product['tcin']
+                except:
+                    tcin = 'Not Found'
+                try:
+                    price_min = product['price']['current_retail']
+                except:
+                    price_min = 'Not Found'
+
+                # tcin_results = get_products_tcin(tcin)
+                # if isinstance(tcin_results, int) and tcin_results > 300:
+                #     break
+                if tcin != 'Not Found':
+                    products_info.append({
+                        "url": str(url),
+                        # "upc": tcin_results['upc'],
+                        "tcin": str(tcin),
+                        # "name": tcin_results['name'],
+                        # "description": tcin_results['description'],
+                        "name": str(name).replace("\"", " "),
+                        "description": str(description).replace('"', '\''),
+                        "image": str(image),
+                        "category": str(category_name),
+                        # "price_max": str(tcin_results['price_max']),
+                        # "price_min": str(tcin_results['price_min']),
+                        "price_min": str(price_min),
+                        "employee": str(vender),
+                    })
+                    insert_into_table(products_info[-1])
+                print("product_info = ", products_info[-1])
+                cnt += 1
+            offset += count
+            time_one_page = datetime.datetime.now() - start_time
+            print("one page time=", time_one_page)
+    return(products_info)
+
+def insert_into_table_old(product_info):
     
     disc = "0"
     sql_query = "SELECT discount FROM discounts WHERE category_name=" + \
@@ -663,7 +848,7 @@ def insert_into_table(product_info):
         cur.execute(sql_query)
         conn.commit()
     
-def get_products_category(categories):
+def get_products_category_old(categories):
     
     count = 28
     products_info = []

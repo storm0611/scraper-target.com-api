5db2e9c3839cb8eec6eb1a8332578a7fa7592e84
devstar0611 <devstar0611@gmail.com>
2022-07-14 15:33:53 -0700

    Update scraper_test.py

--- scraper_test.py
+++ scraper_test.py
@@ -1028,7 +1028,10 @@ def get_products_tcin(tcin):
         try:
             price_min = product_info['price']['reg_retail_min']
         except:
-            price_min = 'Not Found'
+            try:
+                price_min = product_info['price']['reg_retail']
+            except:
+                price_min = 'Not Found'
         # print("product info=", {
         #     "url": url,
         #     "upc": barcode,

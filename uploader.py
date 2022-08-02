bd0d644df9b70e641ad256d350df80b336179c8b
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-08-02 00:02:24 -0700

    sdfds

--- uploader.py
+++ uploader.py
@@ -315,7 +315,7 @@ def uploadToFacebook():
         #         print("image/video button is visible!")
         #         max_timeout = 0
 
-        elements = driver.find_elements(By.TAG_NAME, 'modal')
+        elements = driver.find_elements(By.XPATH, '//*modal')
         print(elements)
         # for element in elements: 
         #     txt = ""

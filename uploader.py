266b0dd663205fe3aedab235e3b2ddb03e8d91a2
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-08-02 20:26:00 -0700

    uploader.py

--- uploader.py
+++ uploader.py
@@ -403,8 +403,8 @@ def uploadToFacebook():
         if len(elements):
             # print(len(elements))
             elements[0].click()
-            print("Video is posted!")
             time.sleep(10)
+            print("Video is posted!")
             
         # max_timeout = 420
         # while max_timeout > 0:

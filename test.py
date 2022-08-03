1a1911414c6867e643f36ce79de4b4ed3e774485
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-08-03 07:27:54 -0700

    Update with google drive

--- test.py
+++ test.py
@@ -1,77 +1,48 @@
+import os
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+import undetected_chromedriver as uc
+import pyautogui
+from pathlib import Path
+import time
 
-print('Enter the gmailid and password')
-gmailId = 'Oleksolo92@gmail.com'
-passWord = 'Temp1234!'
-try:
-    driver = webdriver.Chrome()
-    driver.get(r'https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&flowName=GlifWebSignIn&flowEntry = ServiceLogin')
-    driver.implicitly_wait(15)
+#insert full path to your video file here
+path = os.getcwd() + "\\final.mp4"  # your video path
+print(path)
+##FacebookUser = 'Justsendit@live.com'
 
-    loginBox = driver.find_element(By.XPATH, '//*[@id ="identifierId"]')
-    loginBox.send_keys(gmailId)
-
-    nextButton = driver.find_elements(By.XPATH, '//*[@id ="identifierNext"]')
-    nextButton[0].click()
-
-    passWordBox = driver.find_element(By.XPATH, '//*[@id ="password"]/div[1]/div / div[1]/input')
-    passWordBox.send_keys(passWord)
-
-    nextButton = driver.find_elements(By.XPATH, '//*[@id ="passwordNext"]')
-    nextButton[0].click()
-
-    print('Login Successful...!!')
+options = webdriver.ChromeOptions()
+options.add_argument("--user-data-dir=C:\\Users\\HOPE\\AppData\\Local\\Google\\Chrome\\User Data")
+chromedriver = "./chromedriver"
+capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=options, desired_capabilities=capabilities)
     
-    #driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-# driver = webdriver.Chrome("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe")
-# chrome_options = Options()
-# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
-# #Change chrome driver path accordingly
-# chrome_driver = "./chromedriver.exe"
-# driver = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
-# driver.get(r"https://cosmocode.io/how-to-connect-selenium-to-an-existing-browser-that-was-opened-manually/")
+driver.get("https://drive.google.com/drive/my-drive")
+    
+elements = driver.find_elements(
+        By.CSS_SELECTOR, 'button[aria-label="New"]')
+time.sleep(3)
+print(len(elements))
+elements[0].click()
+time.sleep(2)
+elements = driver.find_elements(
+        By.CSS_SELECTOR, 'div[role="menuitem"][aria-posinset="2"]')
+time.sleep(3)
+print(len(elements))
+elements[0].click()
+time.sleep(3)
 
-    # driver.execute_script(
-    #     '''window.open("https://drive.google.com/drive/folders/1P-qXm0wL8tqKnW3gLI1yQTsTAmQ34LjJ", "_blank");''')
+    # global path
+pyautogui.write(path)
+time.sleep(2)
+pyautogui.press('enter')
+time.sleep(8)
 
+    #checking video upload
+elements = driver.find_elements(
+    By.CSS_SELECTOR, 'div[role="dialog"][aria-label="Uploading 1 item"]')
+time.sleep(2)
+if len(elements):
+    print("Video is posted!")
 
-    # url = driver.command_executor._url
-    # session_id = driver.session_id
-    # print(session_id)
-
-    # driver = webdriver.Remote(command_executor=url, desired_capabilities={})
-    # driver.close()
-    # driver.session_id = session_id
-
-except: 
-    print('Login Failed')
-
-
-
-# import time
-# from selenium import webdriver
-# from selenium.webdriver.chrome.service import Service
-# # Optional argument, if not specified will search path.
-# driver = webdriver.Chrome(
-#     service=Service('C:\Program Files\Google\Chrome\Application\chrome.exe'))
-# driver.get('https://drive.google.com/drive/my-drive')
-# time.sleep(5)  # Let the user actually see something!
-# driver.quit()
-
-# import os
-# from selenium import webdriver
-# from selenium.webdriver.chrome.options import Options
-
-
-# executable_path = "path_to_webdriver"
-# os.environ["webdriver.chrome.driver"] = executable_path
-
-# chrome_options = Options()
-# chrome_options.add_extension('path_to_extension')
-
-# driver = webdriver.Chrome(
-#     executable_path=executable_path, chrome_options=chrome_options)
-# driver.get("http://stackoverflow.com")
-# driver.quit()
+    
--- uploader.py
+++ uploader.py
@@ -463,6 +463,49 @@ def uploadToFacebook():
 
         #/li/div[1]/div/div[1]/span/span
 
+def uploadToGoogle():
+
+    #insert full path to your video file here
+    path = os.getcwd() + "\\final.mp4"  # your video path
+    print(path)
+    ##FacebookUser = 'Justsendit@live.com'
+
+    options = webdriver.ChromeOptions()
+    options.add_argument(
+        "--user-data-dir=C:\\Users\\HOPE\\AppData\\Local\\Google\\Chrome\\User Data")
+    chromedriver = "./chromedriver"
+    capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+    driver = webdriver.Chrome(executable_path=chromedriver,
+                            chrome_options=options, desired_capabilities=capabilities)
+
+    driver.get("https://drive.google.com/drive/my-drive")
+
+    elements = driver.find_elements(
+        By.CSS_SELECTOR, 'button[aria-label="New"]')
+    time.sleep(3)
+    print(len(elements))
+    elements[0].click()
+    time.sleep(2)
+    elements = driver.find_elements(
+        By.CSS_SELECTOR, 'div[role="menuitem"][aria-posinset="2"]')
+    time.sleep(3)
+    print(len(elements))
+    elements[0].click()
+    time.sleep(3)
+
+    # global path
+    pyautogui.write(path)
+    time.sleep(2)
+    pyautogui.press('enter')
+    time.sleep(8)
+
+    #checking video upload
+    elements = driver.find_elements(
+        By.CSS_SELECTOR, 'div[role="dialog"][aria-label="Uploading 1 item"]')
+    time.sleep(2)
+    if len(elements):
+        print("Video is posted!")
+
 def configBrowser():
     global driver
     from pathlib import Path
@@ -474,12 +517,12 @@ def configBrowser():
 ##    options.add_argument('--profile-directory=Default')
 ##
 ##    driver = webdriver.Chrome(options = options)
-    # driver.get("https://www.facebook.com")
-    # driver.execute_script('''window.open("https://www.instagram.com", "_blank");''')
-    # driver.execute_script('''window.open("https://www.tiktok.com", "_blank");''')
+    driver.get("https://www.facebook.com")
+    driver.execute_script('''window.open("https://www.instagram.com", "_blank");''')
+    driver.execute_script('''window.open("https://www.tiktok.com", "_blank");''')
     # driver.execute_script(
     #     '''window.open("https://drive.google.com/drive/folders/1P-qXm0wL8tqKnW3gLI1yQTsTAmQ34LjJ", "_blank");''')
-    driver.get("https://drive.google.com/drive/my-drive")
+    # driver.get("https://drive.google.com/drive/my-drive")
 
 if __name__ == '__main__':
     global driver
@@ -492,8 +535,9 @@ if __name__ == '__main__':
     # uploadToInstagram()
     # uploadToTikTok()
     # uploadToFacebook()
-
-    configBrowser()
+    uploadToGoogle()
+    
+    # configBrowser()
 
 
 

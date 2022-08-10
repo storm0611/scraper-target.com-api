10d63aadd024fcfef97e2971fd54644dd52096f1
devstar0611 <103535364+devstar0611@users.noreply.github.com>
2022-08-10 12:35:23 -0700

    test

--- test.py
+++ test.py
@@ -13,55 +13,62 @@ path = os.getcwd() + "\\final.mp4"  # your video path
 ##FacebookUser = 'Justsendit@live.com'
 # os.system('cmd /k "\"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe\" --remote-debugging-port=9222"')
 
-chrome_options = Options()
-chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+# chrome_options = Options()
+# chrome_options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
 
-# options = webdriver.ChromeOptions()
-# options.add_argument("--user-data-dir=C:\\Users\\justs\\AppData\\Local\\Google\\Chrome\\User Data")
+# chrome_options = webdriver.ChromeOptions()
+# chrome_options.add_argument("--user-data-dir=C:\\Users\\justs\\AppData\\Local\\Google\\Chrome\\User Data")
 chromedriver = "./chromedriver"
-capabilities = webdriver.DesiredCapabilities.CHROME.copy()
-driver = webdriver.Chrome(executable_path=chromedriver,
-                          chrome_options=chrome_options, desired_capabilities=capabilities)
+# capabilities = webdriver.DesiredCapabilities.CHROME.copy()
+# driver = webdriver.Chrome(executable_path=chromedriver,
+                        #   chrome_options=chrome_options, desired_capabilities=capabilities)
+driver = webdriver.Chrome(executable_path=chromedriver)
   
 # Open a new window
 driver.execute_script("window.open('');")
 # Switch to the new window and open URL B
 driver.switch_to.window(driver.window_handles[1])
-driver.get("https://drive.google.com/drive/my-drive")
+driver.get("https://pythonbasics.org/selenium-scroll-down/")
 # driver.get("https://drive.google.com/drive/my-drive")
-time.sleep(3)
-    
-elements = driver.find_elements(
-        By.CSS_SELECTOR, 'button[aria-label="New"]')
-time.sleep(3)
-# print(len(elements))
-elements[0].click()
-time.sleep(2)
-elements = driver.find_elements(
-        By.CSS_SELECTOR, 'div[role="menuitem"][aria-posinset="2"]')
-time.sleep(3)
-# print(len(elements))
-elements[0].click()
-time.sleep(3)
+time.sleep(5)
+scheight = 100
+while scheight > 2:
+    driver.execute_script(
+        "window.scrollTo(0, document.body.scrollHeight/%s);" % scheight)
+    time.sleep(0.1)
+    scheight -= 1
+time.sleep(5)
+# elements = driver.find_elements(
+#         By.CSS_SELECTOR, 'button[aria-label="New"]')
+# time.sleep(3)
+# # print(len(elements))
+# elements[0].click()
+# time.sleep(2)
+# elements = driver.find_elements(
+#         By.CSS_SELECTOR, 'div[role="menuitem"][aria-posinset="2"]')
+# time.sleep(3)
+# # print(len(elements))
+# elements[0].click()
+# time.sleep(3)
 
-    # global path
-pyautogui.write(path)
-time.sleep(2)
-pyautogui.press('enter')
-time.sleep(8)
+#     # global path
+# pyautogui.write(path)
+# time.sleep(2)
+# pyautogui.press('enter')
+# time.sleep(8)
 
-    #checking video upload
-elements = driver.find_elements(
-    By.CSS_SELECTOR, 'div[role="dialog"][aria-label="1 upload complete"]')
-time.sleep(2)
-if len(elements):
-    print("Video is posted!")
-else:
-    elements = driver.find_elements(
-        By.CSS_SELECTOR, 'div[role="dialog"][aria-label="Uploading 1 item"]')
-    time.sleep(2)
-    if len(elements):
-        print("Video is posted!")
+#     #checking video upload
+# elements = driver.find_elements(
+#     By.CSS_SELECTOR, 'div[role="dialog"][aria-label="1 upload complete"]')
+# time.sleep(2)
+# if len(elements):
+#     print("Video is posted!")
+# else:
+#     elements = driver.find_elements(
+#         By.CSS_SELECTOR, 'div[role="dialog"][aria-label="Uploading 1 item"]')
+#     time.sleep(2)
+#     if len(elements):
+#         print("Video is posted!")
 
 driver.close()
     
